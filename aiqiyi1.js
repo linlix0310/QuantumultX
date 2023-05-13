504609335efbbff1809a756de2ac715676fe7530
linlix0310 <36739504+linlix0310@users.noreply.github.com>
2023-05-14 00:49:23 +0800

    Add files via upload

---
+++ aiqiyi1.js
@@ -0,0 +1,112 @@
+var body = $response.body;
+var chxm1023 = JSON.parse(body);
+
+
+
+  chxm1023.content = {	
+	"vip_info" : {
+      "code" : "A00000",
+      "data" : {
+        "qiyi_vip_info" : {
+          "longSuperscript" : "http://static-s.iqiyi.com/common/20220301/qiyue2.0/69/55/b2a7b8af1c8f47ad99b1cb0ba9ed0ab97103194638013294000.png",
+          "superscript" : "http://static-s.iqiyi.com/common/20220301/qiyue2.0/d1/fb/e443befcf4134667819f60edd1e4db996184486938097571652.png",
+          "vipTypeGroup" : "qiyi_vip_info",
+          "longestDeadline" : {
+            "t" : 1688054399000,
+            "date" : "2023年06月29日"
+          },
+          "autoRenew" : "0",
+          "yearExpire" : 1,
+          "rightGroup" : -1,
+          "paidSign" : 1,
+          "surplus" : "16",
+          "type" : "1",
+          "level" : "1",
+          "payType" : "0",
+          "createTime" : {
+            "t" : 1625140438000,
+            "date" : "2021年07月01日"
+          },
+          "vipTypeName" : "黄金VIP会员",
+          "vipType" : "1",
+          "status" : "1",
+          "deadline" : {
+            "t" : 1688054399000,
+            "date" : "2023年06月29日"
+          }
+        }
+      },
+	};
+	
+	"passport" : {
+      "success" : true,
+      "data" : {
+        "icon_list" : {
+          "s" : "https://www.iqiyipic.com/common/fix/headicons/male-130.png",
+          "l" : "https://www.iqiyipic.com/common/fix/headicons/male-130.png",
+          "m" : "https://www.iqiyipic.com/common/fix/headicons/male-130.png"
+        },
+        "userinfo" : {
+          "jointime" : "1625140437",
+          "phone" : "17328358183",
+          "uid" : "1907114364887808",
+          "pru_str" : "1907114364887808",
+          "pwdScore" : 35,
+          "suid" : "1907114364887808",
+          "accountType" : 11,
+          "user_name" : "17328358183",
+          "pru" : 1907114364887808,
+          "icon" : "https://www.iqiyipic.com/common/fix/headicons/male-130.png",
+          "regip" : "111.206.133.34",
+          "nickname" : "用户6c682aa43e300",
+          "email" : "",
+          "status" : 1,
+          "area_code" : "86"
+        },
+        "insecure_account" : 0,
+        "pps_vip_info" : {
+          "level_star" : "01",
+          "vip_type" : 0,
+          "level_moon" : "00",
+          "pps_level" : 1,
+          "vip_remain_day" : 0,
+          "skip_ad" : 1,
+          "pps_bean" : 0,
+          "vip_expire" : "",
+          "user_id" : 1907114364887808,
+          "level_sun" : "00"
+        },
+        "verify_status" : 1,
+        "update_items" : {
+          "CITY" : "false",
+          "NICK" : "false",
+          "GENDER" : "false",
+          "SELF_INTRO" : "false",
+          "BIRTHDAY" : "false",
+          "PROVINCE" : "false",
+          "ICON" : "false"
+        },
+        "qiyi_vip_info" : {
+          "status" : "1",
+          "mobile" : "17328358183",
+          "payType" : "0",
+          "vipType" : "1",
+          "level" : "1",
+          "deadline" : {
+            "date" : "2023年06月29日",
+            "t" : 1688054399000,
+            "date_format" : "2023-06-29"
+          },
+          "surplus" : "16",
+          "type" : "1",
+          "autoRenew" : "0",
+          "name" : "用户6c682aa43e300"
+        }
+      },
+      "code" : "A00000"
+    },
+	
+  }
+ 
+ 
+ $done({body : JSON.stringify(chxm1023)});
