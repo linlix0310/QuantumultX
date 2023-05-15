e7fc54866a475a57ed647c60f0cb704ac5c7e4d9
linlix0310 <36739504+linlix0310@users.noreply.github.com>
2023-05-15 15:56:03 +0800

    Update aiqiyi.js

--- aiqiyi.js
+++ aiqiyi.js
@@ -1,18 +1,19 @@
 var body = $response.body;
 var chxm1023 = JSON.parse(body);
 
-console.log('aiqiyi 1688054399000 in');
+console.log('aiqiyi 1685375999000 in');
 
 chxm1023.data = {	
 	"vip_list" : [{
         "vipTypeGroup" : "vip_info",
         "longestDeadline" : {
-          "t" : 1688054399000
+          "t" : 1685375999000
         },
         "autoRenew" : "0",
         "yearExpire" : 1,
         "rightGroup" : -1,
         "paidSign" : 1,
+	"surplus" : "15",	
         "type" : "1",
         "level" : "1",
         "payType" : "0",
@@ -20,11 +21,11 @@ chxm1023.data = {
         "vipType" : "1",
         "status" : "1",
         "deadline" : {
-          "t" : 1688054399000
+          "t" : 1685375999000
         }
       }],
 	};
 
 
-console.log('aiqiyi 1688054399000 out');
+console.log('aiqiyi 1685375999000 out');
 $done({body : JSON.stringify(chxm1023)});
