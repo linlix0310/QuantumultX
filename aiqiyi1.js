cca77cde863e9595f79a6163c4a768c9d62b7723
linlix0310 <36739504+linlix0310@users.noreply.github.com>
2023-05-16 23:02:58 +0800

    Update aiqiyi1.js

--- aiqiyi1.js
+++ aiqiyi1.js
@@ -26,26 +26,8 @@ console.log('aiqiyi111111 in');
             "date" : "2023年06月29日"
           }
         }
-	};
+	}
 	
-	chxm1023.content.passport = {
-      "data" : {
-        "verify_status" : 1,
-        "qiyi_vip_info" : {
-          "status" : "1",
-          "payType" : "0",
-          "vipType" : "1",
-          "level" : "1",
-          "deadline" : {
-            "date" : "2023年06月29日",
-            "t" : 1688054399000,
-            "date_format" : "2023-06-29"
-          },
-          "type" : "1",
-          "autoRenew" : "0",
-        }
-      }
-  };
- 
+	
  console.log('aiqiyi111111 out');
  $done({body : JSON.stringify(chxm1023)});
